68fc4b4e0cb53a01ed392cb4c919425ae4aec8b1
Brittany Servent <bservent@gmail.com>
2022-02-07 15:25:17 -0800

    api connected

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const api = '56feae0e4801b7e08154c9a4c9aa54cd';
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+      console.log(base);
+      console.log(position)
+    });
+  }
+});
